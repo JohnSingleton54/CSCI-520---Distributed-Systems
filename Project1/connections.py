4d4610eb62a0d8799ac35e25e690c30179a435e8
Grant Nelson <grantnelson1@gmail.com>
2020-03-01 08:28:59 -0700

    Added connections

---
+++ Project1/connections.py
@@ -0,0 +1,108 @@
+#!/usr/bin/env python2
+
+import threading
+import time
+import socket
+import sys
+import select
+
+# helpful links:
+# https://www.tutorialspoint.com/python/python_multithreading.htm
+# https://www.geeksforgeeks.org/python-different-ways-to-kill-a-thread/
+# https://pypi.org/project/multitasking/
+# https://stackoverflow.com/questions/2719017/how-to-set-timeout-on-pythons-socket-recv-method
+
+
+class listener:
+  # This is a class to listen for incoming messages from the given host and port.
+  # This will callback to the given method for handling messages.
+
+  def __init__(self, handleMethod, host, port):
+    # Creates a new listener to the given host and port.
+    super().__init__()
+    self.handleMethod = handleMethod
+    self.host = host
+    self.port = port
+    self.timeToDie = False
+    self.thread = threading.Thread(target=self.__run)
+    self.thread.start()
+
+  def __run(self):
+    # This method run in a separete thread to listen to the socket.
+    # Any message which comes in is passed to handleMethod.
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind((self.host, self.port))
+    s.listen()
+    conn, addr = s.accept()
+    while not self.timeToDie:
+      # Use select to timeout after 1 seconds to check if it is time to die.
+      ready = select.select([conn], [], [], 1)
+      if ready[0]:
+        data = conn.recv(4096)
+        if data:
+          # Got a message send it to the handle method.
+          self.handleMethod(data.decode())
+    # Close connection, close socket, and exit thread
+    conn.close()
+    s.close()
+    return
+
+  def close(self):
+    # Close will stop the listener.
+    # This call will not return until the thread has exited.
+    self.timeToDie = True
+    self.thread.join()
+
+
+class talker:
+  # This is a class to talker to send messages to the given host and port.
+  # This will have a queue of messages which are sent when they can be.
+
+  def __init__(self, host, port):
+    # Creates a new talker to the given host and port.
+    # The given handleLock is used to synchronize calls to handleMethod.
+    super().__init__()
+    self.host = host
+    self.port = port
+    self.timeToDie = False
+    self.queueLock = threading.Lock()
+    self.pendingQueue = []
+    self.thread = threading.Thread(target=self.__run)
+    self.thread.start()
+  
+  def __run(self):
+    # This method run in a separete thread to talk to the socket.
+    # Any messages in the queue will be sent out the socket.
+    connected = False
+    while (not connected) and (not self.timeToDie):
+      try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((self.host, self.port))
+        connected = True
+        while not self.timeToDie:
+          # Check if there are any pending messages and send all of them.
+          self.queueLock.acquire()
+          while self.pendingQueue:
+            message = self.pendingQueue.pop(0)
+            s.sendall(message.encode())
+          self.queueLock.release()
+          # Sleep for a bit to let new messages get pended.
+          time.sleep(1)
+        # Close socket and exit thread
+        s.close()
+        return
+      except Exception as e:
+        # Failed to connect, wait a little bit until the listener is there.
+        time.sleep(1)
+
+  def send(self, message):
+    # Adds the message to the pending messages to be send to the socket.
+    self.queueLock.acquire()
+    self.pendingQueue.append(message)
+    self.queueLock.release()
+
+  def close(self):
+    # Close will stop the talker.
+    # This call will not return until the thread has exited.
+    self.timeToDie = True
+    self.thread.join()
--- Project1/helloRecord.py
+++ Project1/helloRecord.py
@@ -1,112 +1,58 @@
 #!/usr/bin/env python2
 
-# links:
-# https://www.tutorialspoint.com/python/python_multithreading.htm
-# https://www.geeksforgeeks.org/python-different-ways-to-kill-a-thread/
-# https://pypi.org/project/multitasking/
+# Example usage:
+# - In console 1 call "python ./helloRecord.py 127.0.0.1 8080 127.0.0.1 8181"
+# - In console 2 call "python ./helloRecord.py 127.0.0.1 8181 127.0.0.1 8080"
 
 import threading
-import time
-import socket
 import sys
 
-timeToDie = False
-lock = threading.Lock()
+import connections
 
-def criticalPrint(text):
-  global lock
-  lock.acquire()
-  print(text)
-  lock.release()
+record = []
+recordLock = threading.Lock()
+def recordMessage(message):
+  recordLock.acquire()
+  record.append(message)
+  recordLock.release()
 
-class incomingSocketThread (threading.Thread):
-  def __init__(self, host, port):
-    threading.Thread.__init__(self)
-    self.host = host
-    self.port = port
-  def run(self):
-    global timeToDie
-    criticalPrint("Listening to %s %s"%(self.host, self.port))
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.bind((self.host, self.port))
-    criticalPrint("About to Listen")
-    s.listen(0)
-    criticalPrint("About to Accept")
-    conn, addr = s.accept()
-    criticalPrint("Accepted")
-    while not timeToDie:
-      data = conn.recv(1024)
-      if data:
-        criticalPrint("%s says %s"%(addr, data.decode()))
-      time.sleep(1)
-    criticalPrint("Closing Listener")
-    conn.close()
-
-class outgoingSocketThread (threading.Thread):
-  def __init__(self, host, port):
-    threading.Thread.__init__(self)
-    self.host = host
-    self.port = port
-  def run(self):
-    global timeToDie
-    criticalPrint("Talking out to %s %s"%(self.host, self.port))
-    connected = False
-    while (not connected) and (not timeToDie):
-      criticalPrint("Trying to Connect")
-      try:
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.connect((self.host, self.port))
-        criticalPrint("Connected")
-        connected = True
-        while not timeToDie:
-          criticalPrint("Sending Bump")
-          s.sendall('Bump')
-          time.sleep(5)
-        criticalPrint("Closing Talker")
-        s.close()
-      except Exception as e:
-        criticalPrint("Failed to Connect, Retrying in 2 secs")
-        time.sleep(2) # Wait a little bit until the listener is there
-
-class userInputThread (threading.Thread):
-  def __init__(self):
-    threading.Thread.__init__(self)
-  def run(self):
-    global timeToDie
-    while 1:
-      criticalPrint("What would you like to do?")
-      criticalPrint("   1. Say Hello")
-      criticalPrint("   2. Say Goodbye")
-      criticalPrint("   3. Exit")
-      choice = input("Enter your choice: ")
-      if choice == 1:
-        criticalPrint("Hello")
-      elif choice == 2:
-        criticalPrint("Goodbye")
-      elif choice == 3:
-        # Stop other thread
-        criticalPrint("TimeToDie")
-        timeToDie = True
-        break
-      else:
-        criticalPrint("Unknown choice \"%s\". Try again."%(choice))
 
 def main(args):
-  # Create new threads
-  threads = [
-    incomingSocketThread(args[1], int(args[2])),
-    outgoingSocketThread(args[3], int(args[4])),
-    userInputThread()
-  ]
+  host1 = args[1]
+  port1 = int(args[2])
+  listener = connections.listener(recordMessage, host1, port1)
 
-  # Start new Threads
-  for t in threads:
-    t.start()
+  host2 = args[3]
+  port2 = int(args[4])
+  talker = connections.talker(host2, port2)
 
-  # Wait until all threads are done
-  for t in threads:
-    t.join()
-  criticalPrint("Quitting")
+  while 1:
+    print("What would you like to do?")
+    print("   1. Send Message")
+    print("   2. Show Received Messages")
+    print("   3. Exit")
+    choice = int(input("Enter your choice: "))
+    if choice == 1:
+      msg = input("Enter Message: ")
+      talker.send(msg)
+      print()
+
+    elif choice == 2:
+      print("Messages:")
+      recordLock.acquire()
+      for msg in record:
+        print("  "+msg)
+      recordLock.release()
+      print()
+
+    elif choice == 3:
+      print("Closing")
+      talker.close()
+      listener.close()
+      break
+
+    else:
+      print("Unknown choice \"%s\". Try again." % (choice))
 
 if __name__ == "__main__":
     main(sys.argv)
